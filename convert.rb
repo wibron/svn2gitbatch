2d54a9e820bd17d31e5952eb2d7f9fa8f8a4258e
Patrik Wibron <patrik@wibron.nu>
2013-02-02 10:48:00 +0100

    Remove gets.chomp and use variable instead

--- convert.rb
+++ convert.rb
@@ -3,9 +3,8 @@ repos = ['testrepo', 'Another_repo']
 
 # Set the base-url for all repositories
 base_url = 'https://svn.company.com:88/svn/'
-puts 'Enter config file for users: '
-users = gets.chomp
+config = 'users.txt'
 
 repos.each do |repo|
-  system("git svn clone --stdlayout --authors-file=#{users} #{base_url}#{repo}/ #{repo}")
+  system("git svn clone --stdlayout --authors-file=#{config} #{base_url}#{repo}/ #{repo}")
 end
